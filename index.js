79702ac2866664bf1588bc4a701cc79dfd1b7fa0
Andreas Levander <andreas.levander92@gmail.com>
2022-01-28 13:55:18 +0200

    heroku deploy front end

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { 
     if (Object.keys(req.body).length > 0) return JSON.stringify(req.body) 
